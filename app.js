b710f5c8edd18278e6aa6dc34826e0e7974142f6
Shannon Quinn <shannon.quinn91@gmail.com>
2020-11-29 14:19:41 -0500

    Classes, questions, functions

--- app.js
+++ app.js
@@ -12,8 +12,134 @@ const render = require("./lib/htmlRenderer");
 
 
 // Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+function buildTeam(){
+    const teamMembers = [];
+    const managerQs = [
+        {
+            type: "input",
+            name: "mame",
+            message: "What is the managers name?"
+        },
+        {
+            type: "imput",
+            name: "id",
+            message: "What is the managers ID#?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the managers email address?"
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is the managers office number?"
+        }
+    ]
+    const engineerQs = [
+        {
+            type: "input",
+            name: "mame",
+            message: "What is the engineers name?"
+        },
+        {
+            type: "imput",
+            name: "id",
+            message: "What is the engineers ID#?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the engineers email address?"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is the engineers GitHub username?"
+        }
+    ]
+    const internQs = [
+        {
+            type: "input",
+            name: "mame",
+            message: "What is the interns name?"
+        },
+        {
+            type: "imput",
+            name: "id",
+            message: "What is the interns ID#?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the interns email address?"
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "What school does the intern attend?"
+        }
+    ]
+    
+    //prompt user to add an employee (choose manager, engineer, or intern) or not
+    
+    function questionUser() {
+        inquirer.prompt([
+        {
+            type: "list",
+            name: "builder",
+            message: "Would you like to add a team member?",
+            choices: ["Manager", "Engineer", "Intern", "No"]
+        }
+        ]).then((response) => {
+            //if Manager is selected, run the addManager function
+            if (response.choices === "Manager") {
+                addManager();
+            }
+            //if Engineer is selected, run the addEngineer function
+            else if (response.choices === "Engineer") {
+                addEngineer();
+            }
+            //if Intern is selected, run the addIntern function
+            else if (response.choices === "Intern") {
+                addIntern();
+            }
+            //if No is selected, assuming the team is complete, render the HTML page
+            else if (response.choices === "No") {
+                render();
+            }
+        })
+    }
+    // and to create objects for each team member (using the correct classes as blueprints!)
+    //addManager function should use the managerQs to prompt the user, then push the new Manager to the array, then run  
+    function addManager() {
+        inquirer.prompt(managerQs).then((answers) => {
+            const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
+            teamMembers.push(manager);
+            questionUser();
+        })
+    }
+    
+    function addEngineer(){
+        inquirer.prompt(engineerQs).then((answers) => {
+            const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+            teamMembers.push(engineer);
+            questionUser();
+        })
+    }
+    
+    function addIntern() {
+        inquirer.prompt(internQs).then((answers) => {
+            const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+            teamMembers.push(intern);
+            questionUser();
+        })
+    }
+    questionUser();
+}
 
+
+buildTeam();
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
